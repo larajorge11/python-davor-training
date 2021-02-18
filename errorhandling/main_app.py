d5b2f1c85b7fc5cd50266df8b7cdfe8bd5bb596f
Jorge Lara <larajorge11@gmail.com>
2021-02-18 11:32:22 -0500

    Jorge Lara -Adding grade validator

--- errorhandling/main_app.py
+++ errorhandling/main_app.py
@@ -3,5 +3,20 @@ number = input("Type a number: ")
 try:
     number = float(number)
     print("This is your number: " + str(number))
-except:
+except ValueError:
     print("Invalid number")
+
+
+def calculate_student_grade():
+    while True:
+        try:
+            data = float(input("Type the grade: "))
+            if data < 0.0 or data > 5.0:
+                print("Grade is not in the correct range [0 - 5]")
+            else:
+                return data
+        except ValueError:
+            print("Grade is not a correct value")
+
+
+print(f"Your grade is: {calculate_student_grade()}")
