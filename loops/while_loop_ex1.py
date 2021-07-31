62443cdeb2bebe764834af56a08cb22248d59b0a
Jorge Lara <larajorge11@gmail.com>
2021-07-30 21:43:46 -0500

    while loop 1

---
+++ loops/while_loop_ex1.py
@@ -0,0 +1,23 @@
+# condition = True
+#
+# while condition:
+#     print('Ejecutando ciclo while')
+# else:
+#     print('Fin del ciclo')
+
+contador = 0
+
+while contador < 11:
+    print(contador)
+    contador += 1
+else:
+    print('Fin del ciclo while')
+
+
+counter = 5
+
+while counter > 0:
+    print(counter)
+    counter -= 1
+else:
+    print('Fin del ciclo while')
