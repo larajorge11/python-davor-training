4cb65fa1adafc8ca514b82f07733571540416d4e
Jorge Lara <larajorge11@gmail.com>
2021-07-30 21:48:37 -0500

    continue for loop

--- loops/forloopdemo.py
+++ loops/forloopdemo.py
@@ -19,3 +19,12 @@ for n in [1, 2, 3, 4, 5, 6, 7, 8]:
     if n <= 4:
         continue
     print(n)
+
+# for i in range(6):
+#     if i % 2 == 0:
+#         print(f'Valor {i}')
+
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f'Valor {i}')
