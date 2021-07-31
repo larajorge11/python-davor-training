205d9e583f96d3430b96fb8eba251595d7970e37
Jorge Lara <larajorge11@gmail.com>
2021-07-30 22:04:56 -0500

    lists introduction and range exercise

---
+++ lists/lists_01.py
@@ -0,0 +1,47 @@
+nombres = ['Juan', 'Carla', 'Stefani', 'Jacobo', 'Bianca']
+# Imprimir la lista de nombres
+print(nombres)
+# Accediendo a los elementos
+print(nombres[0])
+print(nombres[-1]) # Ultimo elemento de la lista
+
+# Imprimir un rango
+print(nombres[0:2])
+print(nombres[:3])
+print(nombres[2:])
+
+nombres[1] = 'Davor'
+
+for nombre in nombres:
+    print(nombre)
+
+# Preguntar el largo de una lista
+print(f'elementos de la lista {len(nombres)}')
+
+# Adicionar nuevo elemento
+nombres.append('Brandon')
+print(nombres)
+
+# Insertar con un indice especifico
+nombres.insert(1, 'Octavio')
+print(nombres)
+
+# Eliminar un elemento
+nombres.remove('Octavio')
+print(nombres)
+
+# Remover ultimo elemento de lista
+nombres.pop()
+print(nombres)
+
+# Remover un elemento especifico
+del nombres[0]
+print(nombres)
+
+# Limpiar la lista
+nombres.clear()
+print(nombres)
+
+# Remover la lista en memoria
+del nombres
+print(nombres)
---
+++ lists/range_ex1.py
@@ -0,0 +1,4 @@
+for i in range(11):
+    if i % 3 != 0:
+        continue
+    print(i)
