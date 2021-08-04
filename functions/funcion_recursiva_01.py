ba60003ec72dfeeb1e3d19ad204fac6750b94d43
Jorge Lara <larajorge11@gmail.com>
2021-08-04 16:53:57 -0500

    funciones_recursivas_example

--- functions/funcion_recursiva_01.py
+++ functions/funcion_recursiva_01.py
@@ -6,3 +6,13 @@ def factorial(num):
 
 resultado = factorial(5)
 print(resultado)
+
+def imprimir_lista(num):
+    if num <= 0:
+        return 1
+    else:
+        print(num)
+        imprimir_lista(num - 1)
+
+
+imprimir_lista(-4)
