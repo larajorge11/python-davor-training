ba945fd47b8249b8981253718018abce89025025
Jorge Lara <larajorge11@gmail.com>
2021-08-17 17:09:03 -0500

    Add validations constrcutors and setter

--- inheritance/figura_geometrica/figura_geometrica.py
+++ inheritance/figura_geometrica/figura_geometrica.py
@@ -1,7 +1,14 @@
 class FiguraGeometrica:
     def __init__(self, alto, ancho):
-        self.__alto = alto
-        self.__ancho = ancho
+        if alto > 0:
+            self.__alto = alto
+        else:
+            self.__alto = 0
+
+        if ancho > 0:
+            self.__ancho = ancho
+        else:
+            self.__ancho = 0
 
     def __str__(self):
         return f'Figura Geometrica Ancho:{self.__ancho} Alto:{self.__alto}'
@@ -12,7 +19,8 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self.__alto = alto
+        if self._validar_valor(alto):
+            self.__alto = alto
 
     @property
     def ancho(self):
@@ -20,4 +28,8 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self.__ancho = ancho
+        if self._validar_valor(ancho):
+            self.__ancho = ancho
+
+    def _validar_valor(self, valor):
+        return True if valor > 0 else False
--- inheritance/figura_geometrica/test_figura_geometrica.py
+++ inheritance/figura_geometrica/test_figura_geometrica.py
@@ -1,10 +1,22 @@
 from cuadrado import Cuadrado
 from rectangulo import Rectangulo
 
+print('Creacion de cuadrado'.center(50, '-'))
 cuadrado1 = Cuadrado(5, 'rojo')
 print(f'El área del cuadrado es: {cuadrado1.calcular_area()}')
 print(cuadrado1)
 
+print('Creacion de rectangulo'.center(50, '-'))
 rectangulo1 = Rectangulo(3, 5, 'azul')
 print(f'El área del rectangulo es: {rectangulo1.calcular_area()}')
 print(rectangulo1)
+rectangulo2 = Rectangulo(4,-5, 'morado')
+print(f'El área del rectangulo es: {rectangulo2.calcular_area()}')
+print(rectangulo2)
+rectangulo2.ancho = -43
+print(f'El área del rectangulo es: {rectangulo2.calcular_area()}')
+print(rectangulo2)
+rectangulo2.ancho = 6
+print(f'El área del rectangulo es: {rectangulo2.calcular_area()}')
+print(rectangulo2)
+
